6ce961ebb3495f322975c9ab4fcc2bcfbe4fa689
majdiCh016 <majdi.chouat@esprit.tn>
2021-05-21 02:04:51 +0100

    commit of auto logout in case of inactivated user 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AnalyticsService } from './@core/utils/analytics.service';
 import { SeoService } from './@core/utils/seo.service';
+import {AutoLogoutServiceService} from "./utils/services/auto-logout-service.service";
 
 @Component({
   selector: 'ngx-app',
@@ -14,11 +15,12 @@ import { SeoService } from './@core/utils/seo.service';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private analytics: AnalyticsService, private seoService: SeoService, private httpClient: HttpClient) {}
-  
+  constructor(private analytics: AnalyticsService, private seoService: SeoService, private httpClient: HttpClient,private  autoLogoutService: AutoLogoutServiceService) {}
+
   ngOnInit(): void {
     this.analytics.trackPageViews();
     this.seoService.trackCanonicalChanges();
+    this.autoLogoutService.check()
   }
 
 }
